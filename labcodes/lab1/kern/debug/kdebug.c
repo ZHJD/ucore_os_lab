d6e1d2b06306e746f2273b5d8b7e62b1f94f40be
ZHJD <2262040671@qq.com>
2020-04-24 19:24:20 +0800

    complete print stack frame

--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -302,5 +302,19 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+
+    uint32_t ebp = read_ebp();
+    uint32_t eip = read_eip();
+    while(ebp != 0) {
+        cprintf("ebp:0x%08x eip:0x%08x", ebp, eip);
+        cprintf(" args:");
+        for(int i = 0; i < 4; i++) {
+            cprintf("0x%08x ", *(uint32_t*)(ebp + 8 + i * 4));
+        }
+        cprintf("\n");
+        print_debuginfo(eip - 1);
+        ebp = *(uint32_t*)ebp;
+        eip = *(uint32_t*)(ebp + 4);
+    }
 }
 
