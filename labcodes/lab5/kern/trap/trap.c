891633b72cdf19f953f390c485d1685a67675668
ZHJD <2262040671@qq.com>
2020-05-02 15:53:46 +0800

    if you want pass all of tests, it is necessary to comment print_tick()

--- labcodes/lab5/kern/trap/trap.c
+++ labcodes/lab5/kern/trap/trap.c
@@ -216,10 +216,11 @@ trap_dispatch(struct trapframe *tf) {
         syscall();
         break;
     case IRQ_OFFSET + IRQ_TIMER:
+/*
 #if 0
     LAB3 : If some page replacement algorithm(such as CLOCK PRA) need tick to change the priority of pages,
     then you can add code here. 
-#endif
+#endif */
         /* LAB1 YOUR CODE : STEP 3 */
         /* handle the timer interrupt */
         /* (1) After a timer interrupt, you should record this event using a global variable (increase it), such as ticks in kern/driver/clock.c
